e6b61432a6f947ea7db3ce2cd44f7f283e468469
deviousname <35084011+deviousname@users.noreply.github.com>
2022-08-02 15:08:23 -0700

    Update tsp.py

--- tsp.py
+++ tsp.py
@@ -3,8 +3,9 @@ import math
 import time
 import pygame
 
-width, height = 500, 500
-amount_of_cities = 25
+width, height = 512, 512
+amount_of_cities = 16
+
 def distance(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
